fe286eb1e8e68758786c1c95f8f8d8daab99df7c
Santiago Gonzalez <santiagogg1206@gmail.com>
2026-09-21 13:05:49 -0500

    feat: crear interfaz y validacion basica del login

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log("SIGEM iniciado correctamente");
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    if (usuario === "" || password === "") {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
+
+    alert("Datos ingresados correctamente.");
+
+});
